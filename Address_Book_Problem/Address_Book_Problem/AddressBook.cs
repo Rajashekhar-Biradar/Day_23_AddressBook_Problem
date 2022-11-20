f7a477eada6761553b6869f076234615c3428a4e
Rajashekhar-Biradar <rajashekharbiradar41@gmail.com>
2022-11-20 15:48:21 +0530

    Adding person the details to addressBook

---
+++ Address_Book_Problem/Address_Book_Problem/AddressBook.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Address_Book_Problem
+{
+    internal static class AddressBook1
+    {
+        public static void AddPerson()
+        {
+            List<Contacts> personDetails = new List<Contacts>();
+
+            Contacts person = new Contacts();
+            //
+            Console.WriteLine("enter first name");
+            person.FirstName = Console.ReadLine();
+            Console.WriteLine("enter last name");
+            person.LastName = Console.ReadLine();
+            Console.WriteLine("enter Address");
+            person.Address = Console.ReadLine();
+            Console.WriteLine("enter city name");
+            person.City = Console.ReadLine();
+            Console.WriteLine("enter state name");
+            person.state = Console.ReadLine();
+            Console.WriteLine("enter Zip Code");
+            person.ZipCode = Console.ReadLine();
+            Console.WriteLine("enter Phone number");
+            person.PhNo = Console.ReadLine();
+            Console.WriteLine("enter Email");
+            person.Email = Console.ReadLine();
+            personDetails.Add(person);
+
+
+        }
+    }
+}
---
+++ Address_Book_Problem/Address_Book_Problem/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using Address_Book_Problem;
+
+Console.WriteLine("---Welcome to your Address Book--");
+
+//creating object person1 of AddressBook Class()
+
+while (true)
+{
+    Console.WriteLine("Choose one operation to perform:\n 1-Add a Contact \n 2-Exit from the Addressbook");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            AddressBook1.AddPerson();
+            Console.WriteLine("Enter any key to exit");
+            break;
+        case 2:
+            return;
+        default:
+            Console.WriteLine("Please enter a valid input");
+            break;
+    }
+    Console.ReadLine();
+}
